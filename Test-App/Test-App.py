8266efa81970488df425ba888327b6224ea47d43
LIGHT-BIT <110703102+Adrian-Aguilera@users.noreply.github.com>
2023-12-21 16:02:34 -0600

    feat: new functions to modify blogs with python

--- Test-App/Test-App.py
+++ Test-App/Test-App.py
@@ -16,7 +16,7 @@ def get_products():
     return response.json()
 
 #PUT funcion
-def put_funcion(producto_id):
+def put_funcion_products(producto_id):
     endpoint = f'https://{shop}.myshopify.com/admin/api/{Api_version}/products/{producto_id}.json'
     json_data = {
         'product': {
@@ -29,14 +29,58 @@ def put_funcion(producto_id):
 
     return pr_response.json(), pr_response.status_code
 
-if __name__ == "__main__":
-    productos = get_products()
-    with open("test.json", "w") as file:
-        json.dump(productos, file)
 
-    print(productos)
-    id_productos = productos['products'][0]['id']
+#fuction to get id of blogs 
+def get_infoBlogs():
+    endpoint = f'https://{shop}.myshopify.com/admin/api/{Api_version}/blogs.json'
+    response = requests.get(endpoint, headers=headers)
+
+    return response.json()
+#fuction to modify metafields of blogs of shopify
+def modifyBlogs(id_blog):
+    endpoint = f'https://{shop}.myshopify.com/admin/api/{Api_version}/blogs/{id_blog}/metafields.json'
+    metafields_data = {
+        "blog": {
+            "id": id_blog,
+            "metafields": [
+                {
+                    "key": "faq",
+                    "value": json.dumps({
+                        "questions": [
+                            {
+                                "question": "¿Cuál es la pregunta 1?",
+                                "answer": "Esta es la respuesta a la pregunta 1."
+                            },
+                            {
+                                "question": "¿Cuál es la pregunta 2?",
+                                "answer": "Esta es la respuesta a la pregunta 2."
+                            }
+                        ]
+                    }),
+                    "type": "json_string",
+                    "namespace": "faq"
+                }
+            ]
+        }
+    }
+    r = requests.put(endpoint, headers=headers, json=metafields_data)
+    if r.status_code == 200 :
+        return f"successible: {r.json()}"
+    else:
+        #messege = f"requests invalid {r.json()}"
+        messege = r.text
+        return messege
+
+if __name__ == "__main__":
+    blogs_info = get_infoBlogs()
+    id_blogs = blogs_info["blogs"][0]["id"]
+    data_blog = modifyBlogs(id_blogs)
+
+    print(data_blog)
+    #productos = get_products()
+    #print(productos)
+    #id_productos = productos['products'][0]['id']
     #print(id_productos)
-    status = "draft"
-    Put_products, status_code = put_funcion(id_productos)
+    #status = "draft"
+    #Put_products, status_code = put_funcion_products(id_productos)
     #print(Put_products, status_code)
